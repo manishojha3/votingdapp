c255a942e1e0802be300f479f17eb806d0d7cbf4
Manish Ojha <95246186+manishojha3@users.noreply.github.com>
2026-02-17 12:31:14 +0530

    Use environment variables for network configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-;
 /** @type import('hardhat/config').HardhatUserConfig */
-
-
-
 module.exports = {
   solidity: "0.8.24",
   networks: {
     sepolia: {
-      url: "ALCHEMY URL",
-      accounts: ["ACCOUNT PRIVATE KEY"],
+      url: process.env.ALCHEMY_URL,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
